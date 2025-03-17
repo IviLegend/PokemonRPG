368c02e1cc4111404fa6cb1a6f107be7d9a63359
IviLegend <elivilegend@gmail.com>
2025-03-17 12:39:21 +0100

    Added DeltaTime

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -18,7 +18,16 @@ public class GamePanel extends JPanel implements Runnable
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    KeyHandler keyHandler = new KeyHandler();
     Thread gameThread;
+
+    // Frames values
+    int FPS = 60;
+
+    // Player variables
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     /// CONSTRUCTORS
     public GamePanel()
@@ -26,6 +35,8 @@ public class GamePanel extends JPanel implements Runnable
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyHandler);
+        this.setFocusable(true);
     }
 
     /// METHODS
@@ -38,18 +49,53 @@ public class GamePanel extends JPanel implements Runnable
     @Override
     public void run()
     {
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        long timer = 0;
+        int drawCount = 0;
+
         while (gameThread != null)
         {
-            // 1. UPDATE: Update the information of the game
-            update();
-            // 2. DRAW: Draw the screen with the updated information
-            repaint();
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+
+            lastTime = currentTime;
+
+            if (delta >= 1)
+            {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if (timer >= 1000000000) { System.out.println("FPS: " + drawCount); drawCount = 0; timer = 0; }
         }
     }
 
     public void update()
     {
-
+        if (keyHandler.upPressed)
+        {
+            playerY -= playerSpeed;
+        }
+        if (keyHandler.downPressed)
+        {
+            playerY += playerSpeed;
+        }
+        if (keyHandler.rightPressed)
+        {
+            playerX += playerSpeed;
+        }
+        if (keyHandler.leftPressed)
+        {
+            playerX -= playerSpeed;
+        }
     }
 
     public void paintComponent(Graphics graphics)
@@ -57,5 +103,9 @@ public class GamePanel extends JPanel implements Runnable
         super.paintComponent(graphics);
 
         Graphics2D graphics2D = (Graphics2D) graphics;
+
+        graphics2D.setColor(Color.white);
+        graphics2D.fillRect(playerX, playerY, tileSize, tileSize);
+        graphics2D.dispose();
     }
 }
