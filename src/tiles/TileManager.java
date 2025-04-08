544a89fc7079a50d0433c56f7adbc6a52fdb915e
IviLegend <elivilegend@gmail.com>
2025-04-08 09:21:58 +0200

    Created a system that reads .txt files and draws maps with their tiles

--- src/tiles/TileManager.java
+++ src/tiles/TileManager.java
@@ -4,24 +4,68 @@ import main.GamePanel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class TileManager
 {
     /// FIELDS
     public GamePanel gamePanel;
     public Tile[] tiles;
+    int[][] mapTileNum;
 
     /// CONSTRUCTORS
     public TileManager(GamePanel gamePanel)
     {
         this.gamePanel = gamePanel;
         tiles = new Tile[25];
+        mapTileNum = new int[gamePanel.maxScreenCol][gamePanel.maxScreenRow];
 
         getTileImage();
+        loadMap("room01.txt");
     }
 
     /// METHODS
+    public void loadMap(String mapFileName)
+    {
+        try
+        {
+            InputStream inputStream = getClass().getResourceAsStream("/maps/" + mapFileName);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            
+            int col = 0;
+            int row = 0;
+
+            while ((col < gamePanel.maxScreenCol) && (row < gamePanel.maxScreenRow))
+            {
+                String line = bufferedReader.readLine();
+
+                while (col < gamePanel.maxScreenCol)
+                {
+                    String[] numbers = line.split(" ");
+                    int num = Integer.parseInt(numbers[col]);
+
+                    mapTileNum[col][row] = num;
+                    col++;
+                }
+
+                if (col == gamePanel.maxScreenCol)
+                {
+                    col = 0;
+                    row++;
+                }
+            }
+
+            bufferedReader.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
     public void getTileImage()
     {
         try
@@ -40,21 +84,26 @@ public class TileManager
 
     public void draw(Graphics2D graphics2D)
     {
-        int separation = gamePanel.originalTileSize * gamePanel.scale;
+        int col = 0;
+        int row = 0;
+        int x = 0;
+        int y = 0;
 
         // Draw the tilemap
-        for (int i = 0; i < gamePanel.maxScreenCol; i++)
+        while ((col < gamePanel.maxScreenCol) && (row < gamePanel.maxScreenRow))
         {
-            for (int j = 0; j < gamePanel.maxScreenRow; j++)
+            int tileNum = mapTileNum[col][row];
+
+            graphics2D.drawImage(tiles[tileNum].image, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
+            col++;
+            x += gamePanel.tileSize;
+
+            if (col == gamePanel.maxScreenCol)
             {
-                if (j == 0)
-                {
-                    graphics2D.drawImage(tiles[1].image, (i * separation), (0), gamePanel.tileSize, gamePanel.tileSize, null);
-                }
-                else
-                {
-                    graphics2D.drawImage(tiles[0].image, (i * separation), (j * separation), gamePanel.tileSize, gamePanel.tileSize, null);
-                }
+                col = 0;
+                x = 0;
+                row++;
+                y += gamePanel.tileSize;
             }
         }
     }
